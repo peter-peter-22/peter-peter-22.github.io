89747f630f459d287b7075e768a3cb0007838dca
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-01-29 13:01:13 +0100

    remove unused contents

--- download.js
+++
@@ -1,8 +0,0 @@
-function download(content, mimeType, filename){
-    var a = document.createElement('a')
-    var blob = new Blob([content], {type: mimeType})
-    var url = URL.createObjectURL(blob)
-    a.setAttribute('href', url)
-    a.setAttribute('download', filename)
-    a.click()
-  }
