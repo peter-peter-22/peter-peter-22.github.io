5ae9a7a05c3f4464475072effff09890ae11527d
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-01-29 18:03:44 +0100

    minor changes

--- technical.js
+++ technical.js
@@ -14,22 +14,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const visible_from = 70;//the sections become visible when their top is at "visible_from"vh distance from the top of the window
     const visible_from_bottom = 100;
-    
-    const sections = document.querySelectorAll(".scroll_animation");  
+
+    const sections = document.querySelectorAll(".scroll_animation");
 
     function change_visibility() {
         sections.forEach(section => {
             const bottom = section.classList.contains("bottom");
-          if(!bottom&& px_to_vh(section.getBoundingClientRect().top) < visible_from || bottom && px_to_vh(section.getBoundingClientRect().bottom) < visible_from_bottom)
-          section.classList.add("visible");
-          else
-          section.classList.remove("visible");
+            if (!bottom && px_to_vh(section.getBoundingClientRect().top) < visible_from || bottom && px_to_vh(section.getBoundingClientRect().bottom) < visible_from_bottom)
+                section.classList.add("visible");
+            else
+                section.classList.remove("visible");
         });
     }
 
-    function px_to_vh(px)
-    {
-       return 100 * px / window.innerHeight ;
+    function px_to_vh(px) {
+        return 100 * px / window.innerHeight;
     }
 
     window.addEventListener("scroll", function () {
