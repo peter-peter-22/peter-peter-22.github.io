a0ada88fb31550108a7377f788238cac8f1b4ef9
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-01-30 13:19:32 +0100

    update scrolling animations

--- technical.js
+++ technical.js
@@ -12,15 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     //making the sections visible or invisible when they are above or below a certain part of the screen
 
-    const visible_from = 70;//the sections become visible when their top is at "visible_from"vh distance from the top of the window
     const visible_from_bottom = 100;
 
     const sections = document.querySelectorAll(".scroll_animation");
 
     function change_visibility() {
         sections.forEach(section => {
-            const bottom = section.classList.contains("bottom");
-            if (!bottom && px_to_vh(section.getBoundingClientRect().top) < visible_from || bottom && px_to_vh(section.getBoundingClientRect().bottom) < visible_from_bottom)
+            if (px_to_vh(section.getBoundingClientRect().bottom) < visible_from_bottom)
                 section.classList.add("visible");
             else
                 section.classList.remove("visible");
