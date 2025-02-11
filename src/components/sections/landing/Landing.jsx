9a7530c8d0930e8c86e8ab9b736e7bdb71b3cec4
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-02-11 02:18:29 +0100

    minor fixes

--- src/components/sections/landing/Landing.jsx
+++ src/components/sections/landing/Landing.jsx
@@ -1,19 +1,15 @@
+import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
+import { useCallback, useEffect, useRef } from "react";
 import { TitleButtons } from "./Buttons";
-import { Description, Title } from "./Title";
 import { Logos } from "./Logos";
-import { LightBackground, lightBackgrounds } from "../../decorations/backgrounds/LightBackground";
-import { useEffect } from "react";
-import { useRef } from "react";
-import { useCallback } from "react";
+import { Description, Title } from "./Title";
 
 export function Landing() {
-    const containerRef = useLandingFadeOut({ area: 100 });
+    const containerRef = useLandingFadeOut({ area: 200 });
     return (
-        <Box sx={{ position: "relative", position: "sticky", top: 0, zIndex: -1 }}>
-            <LightBackground background={lightBackgrounds.landing} />
+        <Box sx={{ position: "relative", position: "sticky", top: 0 }}>
             <Container ref={containerRef} sx={{ minHeight: "100vh", display: "flex", alignItems: "center" }}>
                 <Stack spacing={5}>
                     <Title />
@@ -36,14 +32,14 @@ function useLandingFadeOut({ area }) {
         let opacity = (area - scrollY) / area
         opacity = Math.max(Math.min(opacity, 1), 0)
         containerRef.current.style.opacity = opacity;
-    },[containerRef])
+    }, [containerRef])
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
             update()
         })
         update()
-        return ()=>{window.removeEventListener("scroll",update)}
+        return () => { window.removeEventListener("scroll", update) }
     })
     return containerRef
 }
--- src/routes/routes/Home.jsx
+++ src/routes/routes/Home.jsx
@@ -3,16 +3,27 @@ import { Landing } from "../../components/sections/landing/Landing";
 import { Languages } from "../../components/sections/languages/Languages";
 import { WhatCanIDo } from "../../components/sections/what can I do/WhatCanIDo";
 import Stack from "@mui/material/Stack";
+import { lightBackgrounds, LightBackground } from "../../components/decorations/backgrounds/LightBackground";
+import Box from "@mui/material/Box";
 
 export function Home() {
     return (
         <>
+            <Background />
             <Landing />
-            <Stack gap={{xs:10,sm:15,md:20}}>
+            <Stack gap={{ xs: 10, sm: 15, md: 20 }} sx={{position:"relative",zIndex:1}}>
                 <AboutMe />
                 <Languages />
                 <WhatCanIDo />
             </Stack>
         </>
+    )
+}
+
+function Background() {
+    return (
+        <Box sx={{ position: "fixed", height: "100%", width: "100%", zIndex: -1 }}>
+            <LightBackground background={lightBackgrounds.landing} />
+        </Box>
     )
 }
