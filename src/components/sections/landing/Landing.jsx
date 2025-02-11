44c4ad65975b4eaf6efa868948ed4bdcb9b9f4c8
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-02-11 01:09:34 +0100

    add landing page animation

--- src/components/sections/landing/Landing.jsx
+++ src/components/sections/landing/Landing.jsx
@@ -1,24 +1,41 @@
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 import { TitleButtons } from "./Buttons";
 import { Description, Title } from "./Title";
 import { Logos } from "./Logos";
 import { LightBackground, lightBackgrounds } from "../../decorations/backgrounds/LightBackground";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 export function Landing() {
+    const containerRef = useLandingFadeOut({ area: 100 });
     return (
-        <Container sx={{ minHeight: "100vh", display: "flex", alignItems: "center" }}>
-            <LightBackground background={lightBackgrounds.landing} sx={{opacity:0}}/>
-            <Stack spacing={5}>
-                <Title />
-                <Description />
-                <Logos />
-                <TitleButtons />
-            </Stack>
-        </Container>
+        <Box sx={{ position: "relative",position:"sticky",top:0,zIndex:-1 }}>
+            <LightBackground background={lightBackgrounds.landing} />
+            <Container ref={containerRef} sx={{ minHeight: "100vh", display: "flex", alignItems: "center" }}>
+                <Stack spacing={5}>
+                    <Title />
+                    <Description />
+                    <Logos />
+                    <TitleButtons />
+                </Stack>
+            </Container>
+        </Box>
     )
 }
 
-function useLandingFadeOut(){
-    
+function useLandingFadeOut({ area }) {
+    const containerRef = useRef()
+    useEffect(() => {
+        window.addEventListener("scroll", (e) => {
+            if (!containerRef.current)
+                return
+            const scrollY = window.scrollY
+            let opacity = (area - scrollY) / area
+            opacity = Math.max(Math.min(opacity, 1), 0)
+            containerRef.current.style.opacity = opacity;
+        })
+    })
+    return containerRef
 }
